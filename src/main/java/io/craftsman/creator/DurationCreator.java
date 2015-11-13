9cab45b2c3b1ecc0e2cfba5bda0bec5aa89549c6
GeBeater <info@gebeat.com>
2015-11-13 13:28:16 +0100

    duration issue *fix creation of Duration with wrong values

--- src/main/java/io/craftsman/creator/DurationCreator.java
+++ src/main/java/io/craftsman/creator/DurationCreator.java
@@ -1,12 +1,11 @@
 package io.craftsman.creator;
 
 import java.time.Duration;
-import java.time.temporal.ChronoUnit;
 
 public class DurationCreator implements Creator<Duration> {
 
     @Override
     public Duration create(Object source) {
-        return Duration.of(((Duration) source).getNano(), ChronoUnit.NANOS);
+        return Duration.parse(source.toString());
     }
 }
