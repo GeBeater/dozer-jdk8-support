f1d1a136f0caaf916a00c89bdb3d2348d1db26a7
Hannes Strydom <hannes.strydom@tsbbank.co.nz>
2017-02-27 14:20:13 +1300

    Add missing test

--- src/test/java/io/craftsman/creator/CreatorFactoryTest.java
+++ src/test/java/io/craftsman/creator/CreatorFactoryTest.java
@@ -26,7 +26,12 @@ public class CreatorFactoryTest {
 
     @Test
     public void testCreateLocalDateTimeCreator() {
-        assertNotNull(objectUnderTest.createLocalDateTimeCreator());
+    	assertNotNull(objectUnderTest.createLocalDateTimeCreator());
+    }
+    
+    @Test
+    public void testCreateZonedDateTimeCreator() {
+        assertNotNull(objectUnderTest.createZonedDateTimeCreator());
     }
 
     @Test
