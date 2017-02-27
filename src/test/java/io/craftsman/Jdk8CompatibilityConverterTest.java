4ae7437cf253a543c0a457f2aff23596518c6357
Hannes Strydom <hannes.strydom@tsbbank.co.nz>
2017-02-27 14:05:36 +1300

    Try import spacing again

--- src/test/java/io/craftsman/Jdk8CompatibilityConverterTest.java
+++ src/test/java/io/craftsman/Jdk8CompatibilityConverterTest.java
@@ -1,13 +1,13 @@
 package io.craftsman;
 
-
 import io.craftsman.creator.*;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.runners.MockitoJUnitRunner; 		 
+import org.mockito.runners.MockitoJUnitRunner;
+
 import java.time.*;
 import java.time.temporal.ChronoUnit;
 
